fa08d328655409c0fee1295ee932eb468e3101be
Awasjo <awasjomail@gmail.com>
2022-11-13 18:12:28 -0500

    CRUD

--- server/routes/incident.js
+++ server/routes/incident.js
@@ -10,5 +10,19 @@ let incidentController = require('../controllers/incident');
 
 router.get('/', incidentController.displayIncidentList);
 
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get('/add', incidentController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add', incidentController.processAddPage);
+
+/* GET Route for displaying the Edit page - UPDATE Operation */
+router.get('/edit/:id', incidentController.displayEditPage);
+
+/* POST Route for processing the Edit page - UPDATE Operation */
+router.post('/edit/:id', incidentController.processEditPage);
+
+/* GET to perform  Deletion - DELETE Operation */
+router.get('/delete/:id', incidentController.performDelete);
 
 module.exports = router;
